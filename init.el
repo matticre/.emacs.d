c38f76f019d446ae72fec7bcbdd73e460c538a8a
matticre <matticre@protonmail.com>
2024-03-17 13:40:07 +0100

    Emacs configs

---
+++ init.el
@@ -0,0 +1,31 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes '(monokai))
+ '(custom-safe-themes
+   '("37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
+ '(global-display-line-numbers-mode t)
+ '(inhibit-startup-screen t)
+ '(ispell-dictionary nil)
+ '(package-selected-packages '(company lsp-mode evil cmake-mode))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Fira Mono" :foundry "CTDB" :slant normal :weight normal :height 120 :width normal)))))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(add-hook 'after-init-hook 'global-company-mode)
