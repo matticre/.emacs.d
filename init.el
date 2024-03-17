9fee09f20bf42877e73a7a481bfc3ff89ba87a89
matticre <matticre@protonmail.com>
2024-03-17 22:15:54 +0100

    Updates

--- init.el
+++ init.el
@@ -5,6 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes '(monokai))
  '(custom-safe-themes
    '("37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" default))
